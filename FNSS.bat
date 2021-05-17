dcb0a0eae4851d24dc5c704f187fb1a1f85d405c
maikyes <80900724+maikyes@users.noreply.github.com>
2021-05-17 09:41:47 -0400

    Add files via upload

--- FNSS.bat
+++ FNSS.bat
@@ -370,14 +370,12 @@ goto menu1
 cls
 echo.
 echo 1: Descargar IPs Registradas
-echo 2: Ver donde esta la IP
 echo.
 set /p ad2="admin password: "
 
 
 
 if %ad2%==1 goto adip
-if %ad2%==2 goto ipfinder2
 
 echo.
 echo INCORRECTO!
@@ -387,92 +385,6 @@ goto adminmaik2
 
 :adip
 powershell.exe -Command wget https://iplogger.org/listfull/4naa72umud6 -OutFile C:\Users\%username%\Desktop\FNSSv3\ip100.png
-goto adminmaik2
-
-
-
-:ipfinder2
-title Maik IP Lookup
-color D
-setlocal ENABLEDELAYEDEXPANSION
-set webclient=webclient
-if exist "%temp%\%webclient%.vbs" del "%temp%\%webclient%.vbs" /f /q /s >nul
-if exist "%temp%\response.txt" del "%temp%\response.txt" /f /q /s >nul
-:menuu2
-cls
-echo Ip lookup/
-
-cls
-echo.
-echo                          Type a IP to lookup
-echo.
-set ip=127.0.0.1
-set /p ip=IP: 
-echo sUrl = "http://ipinfo.io/%ip%/json" > %temp%\%webclient%.vbs
-
-:localip
-cls
-echo set oHTTP = CreateObject("MSXML2.ServerXMLHTTP.6.0") >> %temp%\%webclient%.vbs
-echo oHTTP.open "GET", sUrl,false >> %temp%\%webclient%.vbs
-echo oHTTP.setRequestHeader "Content-Type", "application/x-www-form-urlencoded" >> %temp%\%webclient%.vbs
-echo oHTTP.setRequestHeader "Content-Length", Len(sRequest) >> %temp%\%webclient%.vbs
-echo oHTTP.send sRequest >> %temp%\%webclient%.vbs
-echo HTTPGET = oHTTP.responseText >> %temp%\%webclient%.vbs
-echo strDirectory = "%temp%\response.txt" >> %temp%\%webclient%.vbs
-echo set objFSO = CreateObject("Scripting.FileSystemObject") >> %temp%\%webclient%.vbs
-echo set objFile = objFSO.CreateTextFile(strDirectory) >> %temp%\%webclient%.vbs
-echo objFile.Write(HTTPGET) >> %temp%\%webclient%.vbs
-echo objFile.Close >> %temp%\%webclient%.vbs
-echo Wscript.Quit >> %temp%\%webclient%.vbs
-start %temp%\%webclient%.vbs
-set /a requests=0
-echo.
-rem echo Waiting for API response. . .
-echo  Looking up IP Address. . .
-:checkresponseexists
-if %requests% gtr 7 goto failed
-IF EXIST "%temp%\response.txt" (
-goto response_exist
-) ELSE (
-ping 127.0.0.1 -n 2 -w 1000 >nul
-goto checkresponseexists
-)
-color 5
-:failed
-taskkill /f /im wscript.exe >nul
-del "%temp%\%webclient%.vbs" /f /q /s >nul
-echo.
-echo Did not receive a response from the API.
-echo.
-pause
-goto menuu2
-:response_exist
-cls
-echo.
-echo   IP Results Listed Below
-echo.
-set /a num=(%Random%%%9)+1
-color %num%
-for /f "delims= 	" %%i in ('findstr /i "," %temp%\response.txt') do (
-        set data=%%i
-        set data=!data:,=!
-	set data=!data:""=Not Listed!
-	set data=!data:"=!
-	set data=!data:ip:=IP:		!
-	set data=!data:hostname:=Hostname:	!
-	set data=!data:org:=ISP:		!
-	set data=!data:city:=City:		!
-	set data=!data:region:=State:	!
-	set data=!data:country:=Country:	!
-	set data=!data:postal:=Postal Code:	!
-	set data=!data:loc:=Location:	!
-	set data=!data:timezone:=Timezone:	!
-        echo !data!
-)
-echo.
-del "%temp%\%webclient%.vbs" /f /q /s >nul
-del "%temp%\response.txt" /f /q /s >nul
-pause
 goto adminmaik2
 
 
